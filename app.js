a3679cde681ade0d0b68e29bc4db19812bb31361
xolf <xolf@users.noreply.github.com>
2016-06-14 15:57:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
                 $( href ).toggleClass( 'flied' );
             }
             $('html, body').animate({
-                scrollTop: $( href ).offset().top;
+                scrollTop: $( href ).offset().top
             }, 400, "swing", function () {
                 if(!timeout) $( href + ' .more-text').slideToggle();
                 timeout = setTimeout(function () {
